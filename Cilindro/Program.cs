093f74a360f91b33f0b9975029ffdea337c472c7
André Rosa <andrefrancorosa@gmail.com>
2020-03-05 10:19:01 +0000

    ex4 done

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Cilindro
 {
@@ -6,7 +7,54 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Ola! Vou gerar um cilindro!");
+            Console.WriteLine("Insira uma altura");
+            /// <summary>
+            /// Keeps user inserted value
+            /// </summary>
+            /// <returns></returns>
+            string a = Console.ReadLine();//altura
+            /// <summary>
+            /// Converts user inserted value
+            /// </summary>
+            /// <returns></returns>
+            int A = Convert.ToInt32(a);
+            Console.WriteLine("Insira um raio");
+            /// <summary>
+            /// Keeps user inserted value
+            /// </summary>
+            /// <returns></returns>
+            string r = Console.ReadLine();//raio
+            /// <summary>
+            /// Converts user inserted value
+            /// </summary>
+            /// <returns></returns>
+            int R = Convert.ToInt32(r);
+            /// <summary>
+            /// Calculates value
+            /// </summary>
+            double volume = Math.PI * R * R *A ;//calcula o volume
+            /// <summary>
+            /// calculates value
+            /// </summary>
+            /// <param name="A"></param>
+            /// <returns></returns>
+            double area = 2*Math.PI*R*(R + A);//calcula a area
+            /// <summary>
+            /// Shows value with 1 decimal
+            /// </summary>
+            /// <value></value>
+            string resultadoVolume = $"Volume do cilindro = \u03C0 * raio^2 * altura = {volume:f1}";
+            /// <summary>
+            /// shows value with 2 decimal
+            /// </summary>
+            /// <param name="altura"></param>
+            /// <returns></returns>
+            string resultadoArea = $"Area da superficie do cilindro = 2\u03C0 * raio + (raio + altura) = {area:f2}";
+            Console.WriteLine(resultadoVolume);
+            Console.WriteLine(resultadoArea);
+            Console.WriteLine();
         }
     }
 }
