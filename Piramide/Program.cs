e17abb2b1a012934966924e0aaee4abd0b30003c
André Rosa <andrefrancorosa@gmail.com>
2020-03-05 11:10:09 +0000

    exercicio 9 feito

--- Piramide/Program.cs
+++ Piramide/Program.cs
@@ -6,7 +6,22 @@ namespace Piramide
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Insere o número de andares");
+            string num = Console.ReadLine();
+            int n = Convert.ToInt32(num);
+            int i, spaces;
+
+            for (i = 0; i <= n; i++)//Executado enquanto i for menor ou igual a n
+            {
+                for (spaces = 1; spaces <= n - i; spaces++)//Executado enquanto spaces for menor ou igual a i - n
+                    Console.Write(" ");
+                for (spaces = 1; spaces <= 2 * i - 1; spaces++)//Executado enquanto spaces for menor  ou igual a (2*i) - 1
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+
         }
     }
 }
